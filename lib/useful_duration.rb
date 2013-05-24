f8eb8b226ac5d3b9d0dbac0612f7cdf7d9a2446e
Jordan Scales <scalesjordan@gmail.com>
2013-05-24 17:52:12 -0400

    Fixes #1 - ability to easily define useful durations

--- lib/useful_duration.rb
+++ lib/useful_duration.rb
@@ -2,6 +2,7 @@ require "useful_duration/version"
 require 'active_support/duration'
 require 'active_support/core_ext/time/calculations'
 require 'active_support/core_ext/time/acts_like'
+require 'active_support/core_ext/string/inflections.rb'
 
 class Numeric
 
@@ -19,5 +20,28 @@ class Numeric
     ActiveSupport::Duration.new(self * 11820, [[:seconds, self * 11820]])
   end
   alias :doctor_zhivago :doctor_zhivagos
+
+end
+
+module UsefulDuration
+
+  def useful_duration name, value
+    # Send a `define_method` to Numeric class
+    Numeric.send(:define_method, name) do
+      ActiveSupport::Duration.new(self * value, [[:seconds, self * value]])
+    end
+
+    # Alias the "singular" method with it
+    # For example, useful_duration :fortnights, 1209600
+    #   will create #fortnights and #fortnight
+    Numeric.send(:alias_method, singular_method(name), name)
+  end
+
+  private
+  # Helper function to "singularlize" a method name (as a symbol)
+  # Using ActiveSupport::Inflector::Inflections
+  def singular_method name
+    name.to_s.singularize.to_sym
+  end
 
 end
